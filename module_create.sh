a96cbe89672933a0ddd5d184ce973e031b872cb3
putkoff <57512254+putkoff@users.noreply.github.com>
2023-05-05 17:24:15 -0500

    Update module_create.sh

--- module_create.sh
+++ module_create.sh
@@ -17,171 +17,118 @@ if [ ! -d "$target_dir" ]; then
   exit 1
 fi
 
-# Create the file directory structure
-mkdir -p "$target_dir/$module_name/components/utils/__pycache__"
-mkdir -p "$target_dir/$module_name/components/utils/path_utils"
-mkdir -p "$target_dir/$module_name/components/utils/cmd"
-mkdir -p "$target_dir/$module_name/components/class"
-mkdir -p "$target_dir/$module_name/$module_name.egg-info"
-mkdir -p "$target_dir/$module_name/__pycache__"
-mkdir -p "$target_dir/$module_name/read_me"
-
-# Create the files
-touch "$target_dir/path_map.py"
-touch "$target_dir/$module_name/components/functions.py"
-touch "$target_dir/$module_name/components/utils/__pycache__/class_calls.cpython-310.pyc"
-touch "$target_dir/$module_name/components/utils/__init__.py"
-touch "$target_dir/$module_name/components/utils/path_utils/mapping_dirs.py"
-touch "$target_dir/$module_name/components/utils/path_utils/path_map.py"
-touch "$target_dir/$module_name/components/utils/cmd/cmd.py"
-touch "$target_dir/$module_name/components/utils/class_calls.py"
-touch "$target_dir/$module_name/$module_name.egg-info/PKG-INFO"
-touch "$target_dir/$module_name/$module_name.egg-info/SOURCES.txt"
-touch "$target_dir/$module_name/$module_name.egg-info/dependency_links.txt"
-touch "$target_dir/$module_name/$module_name.egg-info/top_level.txt"
-touch "$target_dir/$module_name/$module_name.egg-info/requires.txt"
-touch "$target_dir/$module_name/__pycache__/custom_utils.cpython-310.pyc"
-touch "$target_dir/$module_name/__pycache__/$module_name.cpython-310.pyc"
-touch "$target_dir/$module_name/__pycache__/__init__.cpython-310.pyc"
+# Create the module directory
+mkdir -p "$target_dir/$module_name/components"
+mkdir -p "$target_dir/$module_name/all_functions"
 touch "$target_dir/$module_name/__init__.py"
-touch "$target_dir/$module_name/read_me/__init__.txt"
-touch "$target_dir/$module_name/read_me/custom_utils.txt"
-touch "$target_dir/$module_name/$module_name.py"
-touch "$target_dir/$module_name/path_map.py"
-touch "$target_dir/$module_name/setup.py"
-touch "$target_dir/$module_name/custom_utils.py"
 
-zenity --info --text "File directory created successfully in: $target_dir"
+# Initialize additional directory imports list
+additional_directory_imports=""
 
-# Select library directories for the path_libraries dictionary
-finished=false
-path_libraries=""
-while [ "$finished" = false ]; do
-  library_dir=$(zenity --file-selection --title "path_libraries dictionary: Select a library directory" --directory)
-  library_name=$(basename "$library_dir")
-  path_libraries+="\"$library_name\":\"$library_dir\","
-  response=$(zenity --question --text="Do you want to add another library directory?" --width=300 --height=100; echo $?)
-  if [ $response -eq 1 ]; then
-    finished=true
-  fi
-done
+# Ask the user if they want to add additional directory imports
+response=$(zenity --question --text="Do you want to add additional directory imports?" --width=300 --height=100; echo $?)
+if [ $response -eq 0 ]; then
+  while true; do
+    additional_import=$(zenity --file-selection --title "Select an additional directory to import" --directory)
 
-path_libraries="{${path_libraries::-1}}"
+    if [ -z "$additional_import" ]; then
+      break
+    fi
 
-# Convert path_libraries JSON string to an associative array
-declare -A path_libraries_array
-path_libraries_array=$(echo "import json; d = json.loads('$path_libraries'); print({k: v for k, v in d.items()})" | python3)
+    additional_directory_imports+="$additional_import,"
 
-library_files=""
-for library_name in "${!path_libraries_array[@]}"; do
-  library_dir="${path_libraries_array[$library_name]}"
-  # Select Python files for importing libraries in each directory
-  selected_files=$(zenity --file-selection --title "Select Python files for importing libraries in $library_name" --file-filter="*.py" --multiple --separator="|" --filename="$library_dir/")
-  if [ -n "$selected_files" ]; then
-    library_files+="$selected_files|"
-  fi
-done
-library_files=${library_files::-1}  # Remove the trailing "|"
-
+    response=$(zenity --question --text="Do you want to add another directory import?" --width=300 --height=100; echo $?)
+    if [ $response -eq 1 ]; then
+      break
+    fi
+  done
+fi
 # Create the main Python file
 module_file_path="$target_dir/$module_name/$module_name.py"
-super_setup_path="$target_dir/$module_name/setup.py"
-super_init_path="$target_dir/$module_name/__init__.py"
-# Write the content to the file
+
 cat > "$module_file_path" << EOL
 import os
 import sys
 import json
-from pathlib import Path
-import custom_utils
-from custom_utils import *
-from components.functions import *
-# Create a function from a string
-def mk_libraries(path_libraries):
-    path_keys = list(path_libraries.keys())
-    for k in range(0,len(path_keys)):
-        key = path_keys[k]
-        change_glob_GPT(key,Path(path_libraries[key]))
-        if str(get_globes(key)) not in sys.path:
-            sys.path.append(str(get_globes(key)))
-path_libraries=$path_libraries
-mk_libraries(path_libraries)
+from collections import defaultdict
+
+# Function to import all modules from a directory
+def import_all_from_folder(folder_path):
+    modules = []
+    functions = defaultdict(list)
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.py'):
+                sys.path.append(root)
+                module_name = file[:-3]
+                module = __import__(module_name)
+                modules.append(module_name)
+
+                for attr_name in dir(module):
+                    attr = getattr(module, attr_name)
+                    if callable(attr) and not attr_name.startswith('_'):
+                        functions[module_name].append(attr_name)
+    return modules, functions
+
+# Import all components
+components_dir = os.path.join(os.path.dirname(__file__), "components")
+modules, functions = import_all_from_folder(components_dir)
+
+# Import additional directories
+additional_directory_imports = "${additional_directory_imports::-1}"
+if additional_directory_imports:
+    additional_directories = additional_directory_imports.split(",")
+    for additional_directory in additional_directories:
+        additional_modules, additional_functions = import_all_from_folder(additional_directory)
+        modules.extend(additional_modules)
+        for key, value in additional_functions.items():
+            functions[key].extend(value)
+
+# Save function lists
+all_functions_path = os.path.join(os.path.dirname(__file__), "all_functions")
+with open(os.path.join(all_functions_path, "all_modules.json"), "w") as f:
+    json.dump(modules, f)
+
+with open(os.path.join(all_functions_path, "all_functions.json"), "w") as f:
+    json.dump(functions, f)
+
+# Find duplicate functions
+duplicates = defaultdict(list)
+for module, func_list in functions.items():
+    for func in func_list:
+        for other_module, other_func_list in functions.items():
+            if module != other_module and func in other_func_list:
+                duplicates[func].append((module, other_module))
+
+# Save duplicates to JSON
+with open(os.path.join(all_functions_path, "all_function_duplicates.json"), "w") as f:
+    json.dump(duplicates, f)
+
 EOL
 
-# Generate import statements and init file contents for each directory
-declare -A init_file_contents
-continue_adding_imports=true
-while $continue_adding_imports; do
-  IFS="|" read -ra library_paths <<< "$library_files"
-  import_statements=""
-  for path in "${library_paths[@]}"; do
-    dirname=$(dirname "$path")
-    filename=$(basename "$path")
-    module_name="${filename%.*}"
-    import_statements+="from $module_name import *\n"
-    init_file_contents["$dirname"]+="from .$module_name import *\n"
-  done
+# Create setup.py file
+setup_file_path="$target_dir/$module_name/setup.py"
 
-  # Append import statements to the main Python file
-  echo -e "$import_statements" >> "$module_file_path"
-
-  # Ask the user if they want to continue adding imports
-  response=$(zenity --question --text="Do you want to add more imports?" --width=300 --height=100; echo $?)
-  if [ $response -eq 1 ]; then
-    continue_adding_imports=false
-  else
-    # Iterate through path_libraries to select Python files for importing libraries
-    for library_name in "${!path_libraries_array[@]}"; do
-      library_dir="${path_libraries_array[$library_name]}"
-
-      # Select Python files for importing libraries in each directory
-      selected_files=$(zenity --file-selection --title "Select Python files for importing libraries in $library_name" --file-filter="*.py" --multiple --separator="|" --filename="$library_dir/")
-      if [ -n "$selected_files" ]; then
-        library_files+="$selected_files|"
-      fi
-    done
-    library_files=${library_files::-1}  # Remove the trailing "|"
-  fi
-done
-
-
-
-# Write the contents of __init__ files
-for dir in "${!init_file_contents[@]}"; do
-  echo -e "${init_file_contents[$dir]}" > "$super_init_path"
-done
-
-# Generate setup.py with extracted dependencies
-
-IFS="|" read -ra library_paths <<< "$library_files"
-dependencies=""
-for path in "${library_paths[@]}"; do
-  file_dependencies=$(grep -E '^import |^from ' "$path" | sort -u | awk '{print $2}' | grep -vE 'os|sys|json|pathlib|re|time|datetime|custom_utils|components.functions' | sed 's/,//g' | tr '\n' ',')
-  dependencies+="$file_dependencies,"
-done
-
-# Remove duplicate dependencies and trailing comma
-IFS="," read -ra unique_dependencies <<< "$(echo "$dependencies" | tr ',' '\n' | sort -u | tr '\n' ',')"
-dependencies=""
-for dep in "${unique_dependencies[@]}"; do
-  if [ -n "$dep" ]; then
-    dependencies+="$dep,"
-  fi
-done
-dependencies=${dependencies%,}
-
-cat > "$super_setup_path" << EOL
+cat > "$setup_file_path" << EOL
 from setuptools import setup, find_packages
 
 setup(
-    name="$module_name",
-    version="0.1",
+    name='$module_name',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        $dependencies
+        # Add your module dependencies here
     ],
+    entry_points={
+        'console_scripts': [
+            '$module_name = $module_name.main_module:main',
+        ],
+    },
 )
+
 EOL
 
-zenity --info --text "Python module created successfully in: $target_dir"
 
+# Change to the module directory and run setup.py
+cd "$target_dir/$module_name"
+python3 setup.py develop --user
